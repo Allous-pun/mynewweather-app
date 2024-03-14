8ab44f13e7fad684de0b63e636acc6bf71397ece
Aloyce CodeCraftsman <125455306+Allous-pun@users.noreply.github.com>
2024-03-14 16:46:07 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,47 @@
+let weather = {
+    apiKey: "d3e9eed916e2ffa4077443d6e3f099e6",
+    fetchWeather : function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q="
+             + city 
+             + "&units=metric&appid=" 
+             + this.apiKey
+        )
+        .then((Response) => Response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed)
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = 
+            "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind Speed: " + speed + "Km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1700x1000/?" + name +"')"
+    },
+    search: function(){
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+};
+
+document
+    .querySelector(".search button").addEventListener("click", function () {
+        weather.search();
+});
+
+document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+        if (event.key == "Enter") {
+            weather.search();
+        }
+});
+
+weather.fetchWeather("Eldoret");
